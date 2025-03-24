592b3b92614c0cf956cfe2a2e53ab17d43d6eac5
Saul  Rodriguez <cristian.rodriguez@ids.com.mx>
2025-03-23 19:39:22 -0600

    feat(saul-rodriguez): index.html modified

---
+++ bbvaApp/pantalla1/main.js
@@ -0,0 +1,11 @@
+import {UsernameInput} from './login-page.js';
+import {PasswordInput} from './login-page.js';
+import {LoginButton} from './login-page.js';
+import {LoginPage} from './login-page.js';
+
+
+
+window.customElements.define('login-page', LoginPage);
+window.customElements.define('username-input', UsernameInput);
+window.customElements.define('password-input', PasswordInput);
+window.customElements.define('login-button', LoginButton);
